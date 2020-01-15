816305ebf2b55cf580ab8691fbac71c6ddbf746d
dominik2561 <dominik.eska000@gmail.com>
2020-01-15 23:23:44 +0100

    Prosze o zaliczenie przedmiotu

---
+++ lab3/66075/server/src/main/java/com/Bikes/model/BikeEventType.java
@@ -0,0 +1,7 @@
+package com.Bikes.model;
+
+public enum BikeEventType {
+    APPEARED,
+    DISAPPEARED,
+    STATION_CHANGED
+}
---
+++ lab3/66075/server/src/main/java/com/Bikes/model/StationEventType.java
@@ -0,0 +1,6 @@
+package com.Bikes.model;
+
+public enum StationEventType {
+    BIKE_NUMBER_INCREASED,
+    BIKE_NUMBER_DECREASED
+}
